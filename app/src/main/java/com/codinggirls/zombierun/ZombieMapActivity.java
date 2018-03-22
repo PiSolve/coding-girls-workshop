c57b0430bf34fdb40f057cb59993c13a5faee092
EXJUSTICE <YIJIE.XU.10@UCL.AC.UK>
2018-03-22 22:29:13 +0800

    booyah initial map commit
    Should be good to go. Havent tested it yet though. Still need to add in the google maps api key into manifest and strings.
    Ideally that shouldnt be public but what the hell

--- app/src/main/java/com/codinggirls/zombierun/ZombieMapActivity.java
+++ app/src/main/java/com/codinggirls/zombierun/ZombieMapActivity.java
@@ -1,19 +1,64 @@
 package com.codinggirls.zombierun;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by renu.yadav on 21/3/18.
- */
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class ZombieMapActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import android.support.v4.app.Fragment;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ZombieMapActivity extends AppCompatActivity implements
+        OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener{
     private static final String PLAYER_NAME = "playerName";
     private String mPlayerName = "";
 
+    //Map Var
+    GoogleMap googleMap;
+    LocationManager locationManager;
+
+    Location mLastLocation;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    double lat, lon;
+    double userlat, userlon;
+    Location userloc;
+    boolean firstZoom= false;
+
+    //Renus old code
     public static void startActivity(String name, Context context) {
         Intent intent = new Intent(context, ZombieMapActivity.class);
         intent.putExtra(PLAYER_NAME, name);
@@ -24,9 +69,219 @@ public class ZombieMapActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getDataFromBundle();
+        setContentView(R.layout.activity_map);
+
+
+        checkLocationPermission();
+
+
     }
+
+
+    //Required code for LocationListener, onConnected to Google Play Services, we will find
+    @Override
+    public void onConnected(Bundle bundle) {
+        checkLocationPermission();
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(100);
+        if (mGoogleApiClient == null || !mGoogleApiClient.isConnected()){
+
+            buildGoogleApiClient();
+            mGoogleApiClient.connect();
+
+        }
+        if (mGoogleApiClient.isConnected()){
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+
+
+        //TODO Upon connection, marker on current location, also used in checkArrival
+
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            lat = mLastLocation.getLatitude();
+            lon = mLastLocation.getLongitude();
+            LatLng loc = new LatLng(lat, lon);
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(loc));
+                /*
+
+                */
+        }
+
+    }
+    @Override
+    public void onConnectionSuspended(int i){
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location){
+        //Heres the code to track user location. One can add markers or do whatever when the user momves.
+        mLastLocation=location;
+        lat  = mLastLocation.getLatitude();
+        lon = mLastLocation.getLongitude();
+        LatLng loc = new LatLng(lat,lon);
+
+
+        //initial zoom
+        if(firstZoom ==false){
+            googleMap.animateCamera(CameraUpdateFactory.zoomTo(17));
+            firstZoom=true;
+        }
+
+
+    }
+
+    public void onConnectionFailed(ConnectionResult connectionResult){
+        buildGoogleApiClient();
+    }
+
+    synchronized void buildGoogleApiClient(){
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    };
+    //Connect and disconnect to ApiClient during start/destroy
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient == null || !mGoogleApiClient.isConnected()){
+
+            buildGoogleApiClient();
+            mGoogleApiClient.connect();
+
+        }
+
+
+
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        mGoogleApiClient.disconnect();
+    }
+
+    //Code required for getMapAsync, part of onMapReadyCallback
+    //GetMapAsync needs hence this callback method, where you can immediately set stuff
+    @Override
+    public void onMapReady(GoogleMap map ) {
+
+
+        this.googleMap = map;
+
+
+        map.setMyLocationEnabled(true);
+        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+        //Custom Map UI set up
+        //disable zoom Controls
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.getUiSettings().setZoomGesturesEnabled(true);
+
+        checkLocationPermission();
+
+        //TODO heres where we can control the amount of zoom necessary to maintain the illusion of spawn and chase
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(17));
+
+
+
+
+    }
+
+    public boolean isGooglePlayServicesAvailable(Activity activity){
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+        int status= api.isGooglePlayServicesAvailable(this);
+        //if we have a problem, return false
+        if(status != ConnectionResult.SUCCESS) {
+            if (api.isUserResolvableError(status)) {
+                api.getErrorDialog(activity, status, 2404).show();
+            }
+            return false;
+
+        }
+        return true;
+    }
+
+    //Check if user allowed permission already
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION=99;
+    public boolean checkLocationPermission(){
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
+        {
+            //No permission allowed, force user to give one
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.CAMERA, Manifest.permission.WRITE_SETTINGS, Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_LOCATION);
+            return false;
+        }else {
+            return true;
+        }
+
+    }
+
+
+    //callback from RequestPermissions() method
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int []grantResults){
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST_LOCATION:{
+                //if request is cancelled result arrays are empty
+                if(grantResults.length>0 && grantResults[0] ==PackageManager.PERMISSION_GRANTED){
+                    //permission granted, so do everything related to locations
+                    if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED)
+                    {
+                        if (mGoogleApiClient == null){
+                            buildGoogleApiClient();
+                        }
+                        googleMap.setMyLocationEnabled(true);
+                    }
+                }else{
+                    //permission denied
+                    Toast.makeText(this,"permission denied, app functionality disabled", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
+
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        //Required CONNECT CALL TO ACTUALLY START FUSED LOCATION API
+
+        if (mGoogleApiClient == null || !mGoogleApiClient.isConnected()){
+
+            buildGoogleApiClient();
+            mGoogleApiClient.connect();
+
+        }
+
+        if (googleMap == null) {
+            SupportMapFragment fm = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+
+            fm.getMapAsync(this);
+        }
+
+
+
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        if (mGoogleApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        }
+
+    }
+
+
 
     private void getDataFromBundle() {
         mPlayerName = getIntent().getStringExtra(PLAYER_NAME);
     }
 }
+
