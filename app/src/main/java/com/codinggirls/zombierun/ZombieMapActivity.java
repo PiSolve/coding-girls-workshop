22e5a3494a3402756755f2a5b2ff7ca3fd81fa5a
PiSolve <renuyadav20@gmail.com>
2018-03-26 15:43:41 +0800

    add moving marker melthod

--- app/src/main/java/com/codinggirls/zombierun/ZombieMapActivity.java
+++ app/src/main/java/com/codinggirls/zombierun/ZombieMapActivity.java
@@ -1,14 +1,21 @@
 package com.codinggirls.zombierun;
 
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Interpolator;
 import android.widget.TextView;
 
 /**
@@ -88,5 +95,38 @@ public class ZombieMapActivity extends AppCompatActivity {
         sendIntent.putExtra(Intent.EXTRA_TEXT, "Hey checkout my score on Zombie run :  " + mPlayerName + " :" + mScore);
         sendIntent.setType("text/plain");
         startActivity(sendIntent);
+    }
+
+
+    // animate  marker based  on any interpolator , (Linear , LinearFixed, or spherical
+    // TODO , once map is ready ,  need to test this @Adrian to take care of map set up 
+    private void animateMarkerToGB(final Marker marker, final LatLng finalPosition, final LatLngInterpolators latLngInterpolator) {
+        final LatLng startPosition = marker.getPosition();
+        final Handler handler = new Handler();
+        final long start = SystemClock.uptimeMillis();
+        final Interpolator interpolator = new AccelerateDecelerateInterpolator();
+        final float durationInMs = 3000;
+
+        handler.post(new Runnable() {
+            long elapsed;
+            float t;
+            float v;
+
+            @Override
+            public void run() {
+                // Calculate progress using interpolator
+                elapsed = SystemClock.uptimeMillis() - start;
+                t = elapsed / durationInMs;
+                v = interpolator.getInterpolation(t);
+
+                marker.setPosition(latLngInterpolator.interpolate(v, startPosition, finalPosition));
+
+                // Repeat till progress is complete.
+                if (t < 1) {
+                    // Post again 16ms later.
+                    handler.postDelayed(this, 16);
+                }
+            }
+        });
     }
 }
